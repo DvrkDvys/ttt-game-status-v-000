259ee66aa8f0356ad70c8216dad482c18e45558c
Jordan Harris <jharris@wework.com>
2018-06-18 17:17:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,21 +7,6 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 
-# describe "./lib/game_status.rb" do
-#   describe 'WIN_COMBINATIONS' do
-#     it 'defines a constant WIN_COMBINATIONS with arrays for each win combination' do
-#       expect(WIN_COMBINATIONS.size).to eq(8)
-
-#       expect(WIN_COMBINATIONS).to include_array([0,1,2])
-#       expect(WIN_COMBINATIONS).to include_array([3,4,5])
-#       expect(WIN_COMBINATIONS).to include_array([6,7,8])
-#       expect(WIN_COMBINATIONS).to include_array([0,3,6])
-#       expect(WIN_COMBINATIONS).to include_array([1,4,7])
-#       expect(WIN_COMBINATIONS).to include_array([2,5,8])
-#       expect(WIN_COMBINATIONS).to include_array([0,4,8])
-#       expect(WIN_COMBINATIONS).to include_array([6,4,2])
-#     end
-#   end
 
   # describe "#won?" do
   #   it 'returns false for an empty board' do
