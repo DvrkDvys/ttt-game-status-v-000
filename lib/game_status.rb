86ab391a197a822e30b8f79a838abe9ba06edbd3
Jordan Harris <jharris@wework.com>
2018-06-18 18:54:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,7 +52,7 @@ end
 def winner(board) 
   if won? == false
     return nil
-  else win_combination = won?(board)
-    return board[win_combination[0]]
+  else i = won?(board)
+    return board[i[0]]
   end
 end
