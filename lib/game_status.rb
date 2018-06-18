476326ca708da57ffba132c94c5cc1f32be7a623
Jordan Harris <jharris@wework.com>
2018-06-18 17:22:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 def won?(board)
-  for each win_combination in WIN_COMBINATIONS
+  # for each win_combination in WIN_COMBINATIONS
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
